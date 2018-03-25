a8d3c71ad3404b61c23b0edd6bbd2dfe1f5cdfe0
Martin Konopka <martkono@gmail.com>
2018-03-25 23:06:06 +0200

    added resolve step to build.bat

--- build.bat
+++ build.bat
@@ -8,6 +8,28 @@ set version=
 if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
+
+set projects=UXI.App
+set projects=%projects%;UXI.Common
+set projects=%projects%;UXI.Common.UI
+set projects=%projects%;UXI.Common.Web
+set projects=%projects%;UXI.Common.WebApi
+set projects=%projects%;UXI.Configuration
+set projects=%projects%;UXI.Configuration.IniStorage
+set projects=%projects%;UXI.CQRS
+set projects=%projects%;UXI.CQRS.EntityFramework
+set projects=%projects%;UXI.IO
+set projects=%projects%;UXI.IO.NTFS
+set projects=%projects%;UXI.OwinServer
+set projects=%projects%;UXI.SystemApi
+set projects=%projects%;UXI.ZIP
+
+for %%p in (%projects%) do (
+	if exist "src\%%p\packages.config" (
+		call "%nuget%" restore "src\%%p\packages.config" -OutputDirectory %cd%\packages -NonInteractive
+	)
+)
+
 REM Build
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" UXI-Libs.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
@@ -15,20 +37,7 @@ REM Package
 mkdir Build
 REM call "%nuget%" pack "src\UXI.App\UXI.App.csproj" -symbols -o Build -p Configuration=%config%
 
-for %%p in (
-   UXI.App
-   UXI.Common
-   UXI.Common.UI
-   UXI.Common.Web
-   UXI.Common.WebApi
-   UXI.Configuration
-   UXI.Configuration.IniStorage
-   UXI.CQRS
-   UXI.CQRS.EntityFramework
-   UXI.IO
-   UXI.IO.NTFS
-   UXI.OwinServer
-   UXI.SystemApi
-   UXI.ZIP
-) do call "%nuget%" pack ".nuget\%%p.nuspec" -symbols -o Build -p Configuration=%config% 
 
+for %%p in (%projects%) do (
+	call "%nuget%" pack ".nuget\%%p.nuspec" -symbols -o Build -p Configuration=%config% 
+)
