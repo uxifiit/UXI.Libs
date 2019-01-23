361e4d6c6b6f410e1f3f8065b6ad26061779af96
Martin Konopka <martkono@outlook.com>
2019-01-23 14:19:49 +0100

    added tests for UXI.Serialization

---
+++ test/UXI.Serialization.Test/Formats/Csv/CsvHeaderNamingContextText.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UXI.Serialization.Formats.Csv
+{
+    [TestClass]
+    public class CsvHeaderNamingContextText
+    {
+
+        [TestMethod]
+        public void GetNextLevel()
+        {
+            // TODO Prepare Header Match function is not called on reference name parameter
+        }
+    }
+}
